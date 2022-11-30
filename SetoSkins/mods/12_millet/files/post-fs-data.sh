9b94a0b8dede192c0abd760919c250cf9f57c4f6
SetoSKins <102343110+SetoSKins@users.noreply.github.com>
2022-11-30 19:34:31 +0800

    Update post-fs-data.sh

--- SetoSkins/mods/12_millet/files/post-fs-data.sh
+++ SetoSkins/mods/12_millet/files/post-fs-data.sh
@@ -3,3 +3,4 @@ MODDIR=${0%/*}
 resetprop -n persist.sys.gz.enable false
 resetprop -n persist.sys.brightmillet.enable false
 resetprop -n persist.sys.powmillet.enable false
+resetprop -n persist.sys.millet.newversion false
