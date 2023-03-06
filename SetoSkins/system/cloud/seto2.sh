cf6ac176399ee60af935cc3ee0b34c9406ec754e
SetoSKins <102343110+SetoSKins@users.noreply.github.com>
2023-03-06 19:21:35 +0800

    Update seto2.sh

--- SetoSkins/system/cloud/seto2.sh
+++ SetoSkins/system/cloud/seto2.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
-c=$(dumpsys window policy | grep mIsScreen|tr -d " "|sed -n '1p')
-until [[ "$c" == "mIsScreenOn=true" ]]
- do
-sleep 30
-done
+while true; do
+if [[ "$(getprop sys.boot_completed)" == "1" ]];then
+sleep 60
 rm -rf /data/adb/service.d/seto.sh
 rm -rf /data/adb/service.d/seto2.sh
 rm -rf /data/media/0/Android/备份温控（请勿删除）
+fi
+done
