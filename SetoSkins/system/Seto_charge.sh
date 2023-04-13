363460c35dac869cb1ca4144f4e31d791b45bb65
SetoSKins <102343110+SetoSKins@users.noreply.github.com>
2023-04-13 20:32:00 +0800

    Update Seto_charge.sh

--- SetoSkins/system/Seto_charge.sh
+++ SetoSkins/system/Seto_charge.sh
@@ -25,6 +25,8 @@ status1=$(cat /sys/class/power_supply/battery/status)
 	  	    echo "50000000" >/sys/class/power_supply/battery/fast_charge_current
 	  	    echo "50000000" >/sys/class/power_supply/battery/current_max
 	  echo '180' > /sys/class/power_supply/bms/temp
+	  echo '180' > /sys/class/power_supply/battery/subsystem/battery/temp
+
 	  echo "50000000" > "$c"
 	    echo "50000000" > "$d"
 		sleep 1s
