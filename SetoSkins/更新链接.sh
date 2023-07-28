33ab6c0e86caceb10a3f9e41166315a41df3f984
柚稚的孩纸 <30484319+zjw2017@users.noreply.github.com>
2023-07-28 18:04:26 +0800

    物归原主

--- SetoSkins/更新链接.sh
+++ SetoSkins/更新链接.sh
@@ -1,1 +1,1 @@
-am start -a 'android.intent.action.VIEW' -d 'https://github.com/zjw2017/SetoSkins_Thermal/releases' >/dev/null 2>&1
+am start -a 'android.intent.action.VIEW' -d 'https://github.com/SetoSKins/SetoSkins_Thermal/releases' >/dev/null 2>&1
