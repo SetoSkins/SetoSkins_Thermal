cdc88a0b6b6a2f88705fae6ee27076db666b8335
SetoSkins <SetoSkins@qq.com>
2026-08-10 23:33:12 +0800

    master

--- SetoSkins/system/Seto_fuckthermal.sh
+++ SetoSkins/system/Seto_fuckthermal.sh
@@ -225,7 +225,9 @@ restart_mi_thermald() {
 
 restart_mi_thermald
 
-if test "$(show_value '修改最大电流数')" == "true"; then
+
+		
+		if test "$(show_value '修改最大电流数')" == "true"; then
 	b=$(grep "^最大电流数=" "$file1" | cut -d "=" -f2)
 	echo "$b" >"$file3"
 	echo "$b" >"$file2"
